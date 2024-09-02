17392390fab5c733589966ff737e6efc0ae5aa6a
MFavian Zaahir <favian.zahir@gmail.com>
2024-09-02 21:50:57 +0700

    finished footer and latest project

--- app/page.jsx
+++ app/page.jsx
@@ -1,10 +1,18 @@
+import About from '@/components/home/About'
+import Footer from '@/components/home/Footer'
 import Hero from '@/components/home/Hero'
+import OurService from '@/components/home/OurService'
+import LatestProject from '@/components/home/LatestProject'
 import React from 'react'
 
 const Home = () => {
   return (
     <div>
       <Hero />
+      <About />
+      <OurService />
+      <LatestProject />
+      <Footer />
     </div>
   )
 }
